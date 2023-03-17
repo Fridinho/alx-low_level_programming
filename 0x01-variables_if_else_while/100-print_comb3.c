8f88e5ea0e8c0c3eddf46697a0fa7c14b62ab4db
Fridinho <dfridinho@yahoo.com>
2023-03-17 16:56:26 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n, m;
 
-	for (n =48; n <= 56; n++)
+	for (n= 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
 		{	
@@ -27,5 +27,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
-		 
+} 
