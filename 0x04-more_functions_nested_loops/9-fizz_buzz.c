60a566422621130e327d4c561c59c9fec1b692b2
Fridinho <dfridinho@yahoo.com>
2023-03-24 08:45:36 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1, i <= 100, i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
@@ -27,14 +27,14 @@ int main(void)
 		}
 			else if (i == 1)
 			{
-				printf(" %d", i);
+				printf("%d", i);
 			}
 
 		else
 		{
-			printf(" %d", i};
+			printf("%d ", i);
 					}
 					printf("\n");
 					return (0);
-					}
-
+	}
+}
