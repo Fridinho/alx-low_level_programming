39532b85dc77370947547dba611d60cfd6337a23
Fridinho <dfridinho@yahoo.com>
2023-03-30 08:17:14 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char buffer[] = "This is a string And this is the rest of the #buffer\n"
+"#cisfun #pointersarefun #infernumisfun\n";
+
+printf("%s\n", buffer);
+printf("---------------------------------\n");
+print_buffer(buffer, sizeof(buffer));
+return (0);
+}
