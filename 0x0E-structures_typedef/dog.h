1fcaec48cc6484607d4ccb71d1165fd4a8b9ee3d
Fridinho <dfridinho@yahoo.com>
2023-04-19 07:24:10 -0700

    0-print_name.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,15 @@
 	 * @name: First member
 	 * @age: Second member
 	 * @owner: Third member
-	 *
 	 * Description: Longer description
 	 */
-	struct dog
+	struct dog()
 	{
+
 		char *name;
 		float age;
 		char *owner;
-	};
+	}
 
 
 	/**
