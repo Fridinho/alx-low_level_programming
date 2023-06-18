026e55bf23fe6b18bced8b0b85a838e0a7508fb0
Fridinho <dfridinho@yahoo.com>
2023-06-18 05:00:42 -0700

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,49 +3,48 @@
 
 size_t print_listint(const listint_t *h)
 {
-    int data;
-    struct dlistint* prev;
-    struct dlistint* next;
-} dlistint_t;
+int data;
+struct dlistint* prev;
+struct dlistint* next;
+}
+{
+dlistint_t* current = head;
+
+printf("Elements of the list: ");
+
+while (current != NULL)
+{
+printf("%d ", current->data);
+current = current->next;
+}
+
+printf("\n");
+}
 
 {
-    dlistint_t* current = head;
-    
-    printf("Elements of the list: ");
-    
-    while (current != NULL) {
-        printf("%d ", current->data);
-        current = current->next;
-    }
-    
-    printf("\n");
+dlistint_t* node1 = (dlistint_t*)malloc(sizeof(dlistint_t));
+dlistint_t* node2 = (dlistint_t*)malloc(sizeof(dlistint_t));
+dlistint_t* node3 = (dlistint_t*)malloc(sizeof(dlistint_t));
+
+{
+node1->data = 1;
+node2->data = 2;
+node3->data = 3;
+
+node1->prev = NULL;
+node1->next = node2;
+
+node2->prev = node1;
+node2->next = node3;
+
+node3->prev = node2;
+node3->next = NULL;
+
+free(node1);
+free(node2);
+free(node3);
 }
 
-int main() {
-
-    dlistint_t* node1 = (dlistint_t*)malloc(sizeof(dlistint_t));
-    dlistint_t* node2 = (dlistint_t*)malloc(sizeof(dlistint_t));
-    dlistint_t* node3 = (dlistint_t*)malloc(sizeof(dlistint_t));
-    
-
-    node1->data = 1;
-    node2->data = 2;
-    node3->data = 3;
-    
-
-    node1->prev = NULL;
-    node1->next = node2;
-    
-    node2->prev = node1;
-    node2->next = node3;
-    
-    node3->prev = node2;
-    node3->next = NULL;
-    
- 
-    free(node1);
-    free(node2);
-    free(node3);
-    
-    return 0;
+return 0;
 }
+
