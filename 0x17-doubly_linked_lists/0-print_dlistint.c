ea638e06ccc998700c29b2e5fc79a26b3aaa3300
Fridinho <dfridinho@yahoo.com>
2023-06-18 05:35:32 -0700

    o

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t print_listint(const listint_t *h)
-{
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+
 int data;
 struct dlistint* prev;
 struct dlistint* next;
-}
+{
+dlistint_t;
+void printDList(dlistint_t* head)
 {
 dlistint_t* current = head;
-
 printf("Elements of the list: ");
 
 while (current != NULL)
@@ -19,30 +28,6 @@ current = current->next;
 }
 
 printf("\n");
-}
-
-{
-dlistint_t* node1 = (dlistint_t*)malloc(sizeof(dlistint_t));
-dlistint_t* node2 = (dlistint_t*)malloc(sizeof(dlistint_t));
-dlistint_t* node3 = (dlistint_t*)malloc(sizeof(dlistint_t));
-
-{
-node1->data = 1;
-node2->data = 2;
-node3->data = 3;
-
-node1->prev = NULL;
-node1->next = node2;
-
-node2->prev = node1;
-node2->next = node3;
-
-node3->prev = node2;
-node3->next = NULL;
-
-free(node1);
-free(node2);
-free(node3);
 }
 
 return 0;
